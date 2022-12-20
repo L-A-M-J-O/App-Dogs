45fe60065d5f5a01b16dc33a939916cb57eafcc2
L-A-M-J-O <lamjo30@gmail.com>
2022-12-20 19:10:01 +0100

    Creando modelos

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -4,9 +4,40 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('dog', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull:false,
+      primaryKey: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    height: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    weight: {
+      type: DataTypes.NUMBER,
+      allowNull: false,
+    },
+    life_span: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    origin: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    temperament: {
+      type: DataTypes.ENUM("Stubborn, Curious, Playful, Adventurous, Active, Fun-loving"),
+      allowNull: false,
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: true,
+    }
   });
 };
