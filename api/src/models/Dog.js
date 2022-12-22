9414ffe6089b6fdbc532a995a18e2aff20f1831c
L-A-M-J-O <lamjo30@gmail.com>
2022-12-22 15:48:01 +0100

    cambios

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -10,42 +10,5 @@ module.exports = (sequelize) => {
       allowNull:false,
       primaryKey: true,
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    height: {
-      type: DataTypes.NUMBER,
-      allowNull: false,
-    },
-    weight: {
-      type: DataTypes.NUMBER,
-      allowNull: false,
-    },
-    life_span: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    origin: {
-      type: DataTypes.STRING,
-      allowNull: true,
-    },
-    temperament: {
-      id: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        allowNull:false,
-        primaryKey: true,
-      },
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-    },
-    image: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      defaultValue: true,
-    }
   });
 };
